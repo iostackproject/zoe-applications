358396d741925e78bf3ff702d31b8ee135a9231d
Francesco Pace <pace.francesco.g@gmail.com>
2017-05-08 10:36:14 +0200

    Fix paths
    Signed-off-by: Francesco Pace <pace.francesco.g@gmail.com>

--- frameworks/spark2/images/spark2-jupyter-notebook-ibm/files/start-notebook.sh
+++ frameworks/spark2/images/spark2-jupyter-notebook-ibm/files/start-notebook.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-cat /opt/spark-defaults.conf | sed -e "s/XXX_DRIVER_MEMORY/$SPARK_DRIVER_RAM/" > ${SPARK_HOME}/conf/spark-defaults.conf
+#cat /opt/spark-defaults.conf | sed -e "s/XXX_DRIVER_MEMORY/$SPARK_DRIVER_RAM/" > ${SPARK_HOME}/conf/spark-defaults.conf
 #cat /opt/core-site.xml | sed -e "s/XXX_NAMENODE_HOST/$NAMENODE_HOST/" > ${HADOOP_HOME}/etc/hadoop/core-site.xml
-cp /opt/hdfs-site.xml ${HADOOP_HOME}/etc/hadoop/
+cp ${SPARK_HOME}/conf/hdfs-site.xml ${HADOOP_HOME}/etc/hadoop/
 
 #HADOOP_USER_NAME=root /opt/hadoop/bin/hdfs dfs -mkdir /user/$NB_USER
 #HADOOP_USER_NAME=root /opt/hadoop/bin/hdfs dfs -chown $NB_USER /user/$NB_USER
